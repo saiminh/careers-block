0d66c4ad6194a64def09d2e273b60c3ee59249c5
saiminh <saiminh@gmail.com>
2023-04-10 16:44:59 +0200

    initial

---
+++ careers-block.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Plugin Name:       Careers Block
+ * Description:       Example block scaffolded with Create Block tool.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       careers-block
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+
+ function render_career_meta() {
+  $id = get_the_ID();
+  $meta = get_metadata( 'post', $id );
+  if ( isset($meta['duree'][0]) ) {
+    $duration = $meta['duree'][0];
+    $content = '<div class="role"><span class="label">Role:</span> '.sprintf($duration).'</div>';
+  }
+  if ( isset($meta['location'][0]) ) {
+    $location = $meta['location'][0];
+    $content = $content.'<div class="location"><span class="label">Location:</span> '.sprintf($location).'</div>';
+  }
+  if ( isset($meta['date_de_debut'][0]) ) {
+    $startdate = $meta['date_de_debut'][0];
+    $content = $content.'<div class="startdate"><span class="label">Start date:</span> '.sprintf($startdate).'</div>';
+  }
+  // if ( isset($meta['type_d_ejob'][0]) ) {
+  //   $department = $meta['type_d_ejob'][0];
+  //   $content = $content.'<div class="role">Department: '.sprintf($department).'</div>';
+  // }
+  // if ( isset($meta['destinataire'][0]) ) {
+  //   $destination = $meta['destinataire'][0];
+  //   $content = $content.'<div class="destination">Destination: '.sprintf($destination).'</div>';
+  // }
+  if ( isset($meta['lien_different'][0]) ) {
+    $pdflink = $meta['lien_different'][0];
+
+    $buttons = '
+      <div class="wp-block-buttons is-layout-flex career-buttons">
+        <div class="wp-block-button is-style-outline">
+          <a 
+            class="wp-block-button__link wp-element-button pdf-link" 
+            style="
+              padding-top:10px;
+              padding-right:20px;
+              padding-bottom:10px;
+              padding-left:20px;
+              background-color: transparent;
+              color: currentColor;
+              border-width: 1px;"
+            target="_blank"
+            href="'.sprintf($pdflink).'">
+            Learn More
+          </a>
+        </div>
+      <div class="wp-block-button is-style-fill">
+        <a 
+          class="wp-block-button__link wp-element-button apply-link" 
+          style="
+            padding-top:10px;
+            padding-right:20px;
+            padding-bottom:10px;
+            padding-left:20px;"
+          href="/apply">
+          Apply
+        </a>
+      </div>
+    </div>';
+    $content = $content.$buttons;
+
+    $content = "<div class='career-data'><h3 class='wp-block-post-title career-title'>".get_the_title( $id )."</h3>".$content."</div>";
+  }
+  return $content;
+}
+function create_block_careers_block_block_init() {
+  register_block_type( __DIR__ . '/build', array(
+    'render_callback' => 'render_career_meta',
+  ) );
+}
+add_action( 'init', 'create_block_careers_block_block_init' );
+
+
+// register custom meta tag field
+function register_post_meta_careers() {
+  register_post_meta( 'careers', 'type_d_ejob', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+  register_post_meta( 'careers', 'duree', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+  register_post_meta( 'careers', 'date_de_debut', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+  register_post_meta( 'careers', 'location', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+  register_post_meta( 'careers', 'destinataire', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+  register_post_meta( 'careers', 'lien_different', array(
+      'show_in_rest' => true,
+      'single' => true,
+      'type' => 'string'
+  ) );
+}
+add_action( 'init', 'register_post_meta_careers' );
---
+++ src/edit.js
@@ -0,0 +1,138 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, MediaPlaceholder } from '@wordpress/block-editor';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useState } from '@wordpress/element';
+import { TextControl } from '@wordpress/components';
+import { store as coreStore } from '@wordpress/core-data';
+import './editor.scss';
+
+export default function Edit(
+  {
+    context: { postType, postId, queryId },
+  }
+) {
+
+  const { meta, title } = useSelect(
+    ( select ) => {
+      const { getEditedEntityRecord } =
+      select( coreStore );
+      const _meta = getEditedEntityRecord(
+        'postType',
+        postType,
+        postId
+        )?.meta;
+      const _title = getEditedEntityRecord(
+        'postType',
+        postType,
+        postId
+        )?.title;
+        return {
+          meta: _meta,
+          title: _title
+        };
+      },
+      [ postType, postId ]
+      );
+
+      
+  const { editEntityRecord } = useDispatch( coreStore );
+  const [ location, setLocation ] = useState( meta.location );
+  const [ duration, setDuration ] = useState( meta.duree );
+  const [ startdate, setStartDate ] = useState( meta.date_de_debut );
+  const [ pdf, setPdf ] = useState( meta.lien_different );
+
+  const updateMetaValue = ( newValue, field ) => {
+    if (field == 'location') {
+      const newMeta = { ...meta, location: newValue };
+      editEntityRecord( 'postType', postType, postId, {
+        meta: newMeta,
+      } );
+      setLocation( newValue );
+    }
+    if (field == 'duration' ) { 
+      const newMeta = { ...meta, duree: newValue };
+      editEntityRecord( 'postType', postType, postId, {
+        meta: newMeta,
+      } );
+      setDuration( newValue );
+    }
+    if (field == 'date_de_debut' ) { 
+      const newMeta = { ...meta, date_de_debut: newValue };
+      editEntityRecord( 'postType', postType, postId, {
+        meta: newMeta,
+      } );
+      setStartDate( newValue );
+    }
+    if (field == 'lien_different' ) { 
+      const newMeta = { ...meta, lien_different: newValue };
+      editEntityRecord( 'postType', postType, postId, {
+        meta: newMeta,
+      } );
+      setPdf( newValue );
+    }
+  };
+  const isDescendentOfQueryLoop = Number.isFinite( queryId );
+
+  if ( isDescendentOfQueryLoop ) {
+    return (
+      <div className='career-data'>  
+        <h3 className='wp-block-post-title career-title'>{title}</h3>
+        <div className='location'><span className='label'>Location:</span> {meta.location}</div>
+        <div className='role'><span className='label'>Role:</span> {meta.duree}</div>
+        <div className='startdate'><span className='label'>Start date:</span> {meta.date_de_debut}</div>
+        <div className="wp-block-buttons is-layout-flex wp-container-8">
+            <div className="wp-block-button is-style-outline">
+              <a 
+                className="wp-block-button__link wp-element-button pdf-link" 
+                target="_blank"
+              >
+                Learn More
+              </a>
+            </div>
+          <div className="wp-block-button is-style-fill">
+            <a 
+              className="wp-block-button__link wp-element-button apply-link" 
+            >
+              Apply
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+
+    return (
+      <div { ...useBlockProps( { className: 'career-data' }) }>
+        <TextControl
+          label={ __( 'Location', 'career-block' ) }
+          value={ location }
+          onChange={ ( newValue ) => updateMetaValue( newValue, 'location' ) } 
+        />
+        <TextControl
+          label={ __( 'Role', 'career-block' ) }
+          value={ duration }
+          onChange={ ( newValue ) => updateMetaValue( newValue, 'duration' ) } 
+        />
+        <TextControl
+          label={ __( 'Start Date', 'career-block' ) }
+          value={ startdate }
+          onChange={ ( newValue ) => updateMetaValue( newValue, 'date_de_debut' ) } 
+        />
+        <MediaPlaceholder
+          icon={ 'pdf' }
+          accept="application/pdf"
+          allowedTypes = { [ 'application/pdf' ] }
+          onSelect = {
+              ( el ) => {
+                  updateMetaValue( el.url, 'lien_different' );
+              }
+          }
+          multiple = { false }
+          labels = { { title: pdf, instructions: 'Upload or choose a PDF from the Media Library' } }
+        >
+        </MediaPlaceholder>
+      </div>
+    );
+  }
+  
+}
