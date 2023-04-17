5c9b53e8a54af16f1682fdc7d3ab6a2ebc8fb193
saiminh <saiminh@gmail.com>
2023-04-17 16:01:07 +0200

    production

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '87815187578a91f6c21c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '733b61d5828cca46af10');
